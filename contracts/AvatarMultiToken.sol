d7406d6d25d5c77ca0a576a6d25f280a79cf43a4
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-15 23:06:58 +0900

    ERC1155準拠のAvatar Contractを作る

---
+++ contracts/AvatarMultiToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/erc1155/ERC1155.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+
+contract AvatarMultiToken {
+    
+}
+
